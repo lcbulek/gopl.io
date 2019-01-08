52e42e85f7f3dea47d002c59973bd7506631890c
lcbulek <luisbulek@gmail.com>
2019-01-08 18:33:10 -0200

    "lissajous examples"

--- ch1/lissajou2/main.go
+++ ch1/lissajou2/main.go
@@ -1,10 +1,3 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// Run with "web" command-line argument for web server.
-// See page 13.
-//!+main
-
 // Lissajous generates GIF animations of random Lissajous figures.
 // Lissajou2 to run web server,
 package main
